f453f3caba357a365052ebe08a86299b2cab0930
abdulaziz <aazizalamri5@gmail.com>
2024-07-31 11:51:34 +0300

    added print(mybranch)

--- bin/test_project.dart
+++ bin/test_project.dart
@@ -2,4 +2,12 @@ import 'package:test_project/test_project.dart' as test_project;
 
 void main(List<String> arguments) {
   print('Hello world: ${test_project.calculate()}!');
+
+  print(add(1,2));
+
+  print('new branch');
+}
+
+add(int x, int y){
+return x+y;
 }
